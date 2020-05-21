56c4790a08860307ffff454ee57c2ec55bf0b1e0
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-21 18:26:07 +0200

    Adding interfaces IEntry and IObjective

---
+++ src/main/java/fr/pederobien/minecraftscoreboards/interfaces/IEntry.java
@@ -0,0 +1,39 @@
+package fr.pederobien.minecraftscoreboards.interfaces;
+
+import org.bukkit.entity.Player;
+
+public interface IEntry {
+
+	/**
+	 * Get the old value of this entry. This value is used to clear or reset the player's objective.
+	 * 
+	 * @return The old value of this entry.
+	 */
+	String getOldValue();
+
+	/**
+	 * @return The current value of this entry. This value is displayed on player's objective.
+	 */
+	String getCurrentValue();
+
+	/**
+	 * Update this entry. The old value equals the current value and the current value is updated.
+	 * 
+	 * @param player The player whose scoreboard is updated.
+	 */
+	void update(Player player);
+
+	/**
+	 * Get the score of this entry. This score represents the line number of this entry in an objective.
+	 * 
+	 * @return The score of this entry.
+	 */
+	int getScore();
+
+	/**
+	 * Set the score of this entry. The score represents the line number of this entry in an objective.
+	 * 
+	 * @param score The new line number of this entry.
+	 */
+	void setScore(int score);
+}
