06de5b2239968b40a13062926971330376b25084
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-23 18:05:11 +0200

    Adding interface IAutoUpdateObjective and its implementation AutoUpdateObjective

---
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/AutoUpdateObjective.java
@@ -0,0 +1,81 @@
+package fr.pederobien.minecraftscoreboards.impl;
+
+import java.util.function.Consumer;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scoreboard.DisplaySlot;
+
+import fr.pederobien.minecraftmanagers.PlayerManager;
+import fr.pederobien.minecraftmanagers.ScoreboardManager;
+import fr.pederobien.minecraftscoreboards.interfaces.IAutoUpdateEntry;
+import fr.pederobien.minecraftscoreboards.interfaces.IAutoUpdateObjective;
+import fr.pederobien.minecraftscoreboards.interfaces.IEntry;
+
+public class AutoUpdateObjective extends Objective implements IAutoUpdateObjective {
+	private Plugin plugin;
+	private boolean isActivated;
+
+	public AutoUpdateObjective(String name, String displayName, Plugin plugin) {
+		super(name, displayName);
+		this.plugin = plugin;
+	}
+
+	public AutoUpdateObjective(String name, String displayName, DisplaySlot displaySlot, Plugin plugin) {
+		super(name, displayName, displaySlot);
+		this.plugin = plugin;
+	}
+
+	public AutoUpdateObjective(String name, String displayName, String criteria, DisplaySlot displaySlot, Plugin plugin) {
+		super(name, displayName, criteria, displaySlot);
+		this.plugin = plugin;
+	}
+
+	@Override
+	public Plugin getPlugin() {
+		return plugin;
+	}
+
+	@Override
+	public void addEntry(IAutoUpdateEntry entry) {
+		super.addEntry(entry);
+		entry.setObjective(this);
+	}
+
+	@Override
+	public void removeEntry(IAutoUpdateEntry entry) {
+		super.removeEntry(entry);
+		entry.setObjective(null);
+		entry.setActivated(false);
+	}
+
+	@Override
+	public void register() {
+		action(entry -> getPlugin().getServer().getPluginManager().registerEvents(entry, getPlugin()));
+	}
+
+	@Override
+	public void initialize() {
+		PlayerManager.getPlayers().forEach(player -> {
+			action(entry -> {
+				getObjective().get().getScore(entry.initialize(player)).setScore(entry.getScore());
+			});
+			ScoreboardManager.setPlayerScoreboard(player, getScoreboard().get());
+		});
+	}
+
+	@Override
+	public boolean isActivated() {
+		return isActivated;
+	}
+
+	@Override
+	public void setActivated(boolean isActivated) {
+		this.isActivated = isActivated;
+		action(entry -> entry.setActivated(isActivated));
+	}
+
+	private void action(Consumer<IAutoUpdateEntry> consumer) {
+		for (IEntry entry : entries())
+			consumer.accept((IAutoUpdateEntry) entry);
+	}
+}
