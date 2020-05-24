ed4bcb90806d1a5249ba326e37e9142473f8940c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-24 10:07:13 +0200

    Adding classes AutoUpdateEntityKilledEntry and AutoUpdateMinedBlockEntry

---
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/entries/AutoUpdateEntityKilledEntry.java
@@ -0,0 +1,39 @@
+package fr.pederobien.minecraftscoreboards.impl.entries;
+
+import org.bukkit.Statistic;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class AutoUpdateEntityKilledEntry extends AutoUpdateStatisticEntry {
+
+	/**
+	 * Create an entry updated when an entity of the given {@link EntityType} is killed.
+	 * 
+	 * @param score      The line number of this entry.
+	 * @param entityType The type of entity used to update this entry.
+	 * @param before     The sequence of characters to be displayed before the player statistic.
+	 * @param after      The sequence of characters to be displayed after the player statistic.
+	 */
+	public AutoUpdateEntityKilledEntry(int score, EntityType entityType, String before, String after) {
+		super(score, Statistic.KILL_ENTITY, entityType, before, after);
+	}
+
+	/**
+	 * Create an entry updated when an entity of the given {@link EntityType} is killed.
+	 * 
+	 * @param score      The line number of this entry.
+	 * @param entityType The type of entity used to update this entry.
+	 * @param before     The sequence of characters to be displayed before the player statistic.
+	 */
+	public AutoUpdateEntityKilledEntry(int score, EntityType entityType, String before) {
+		super(score, Statistic.KILL_ENTITY, entityType, before);
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onEntityDeathEvent(EntityDeathEvent event) {
+		if (getEntityType().equals(event.getEntityType()))
+			update();
+	}
+}
---
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/entries/AutoUpdateMinedBlockEntry.java
@@ -0,0 +1,39 @@
+package fr.pederobien.minecraftscoreboards.impl.entries;
+
+import org.bukkit.Material;
+import org.bukkit.Statistic;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class AutoUpdateMinedBlockEntry extends AutoUpdateStatisticEntry {
+
+	/**
+	 * Create an entry updated when a player mine a block of the given {@link Material}.
+	 * 
+	 * @param score      The line number of this entry.
+	 * @param entityType The type of entity used to update this entry.
+	 * @param before     The sequence of characters to be displayed before the player statistic.
+	 * @param after      The sequence of characters to be displayed after the player statistic.
+	 */
+	public AutoUpdateMinedBlockEntry(int score, Material material, String before, String after) {
+		super(score, Statistic.MINE_BLOCK, material, before, after);
+	}
+
+	/**
+	 * Create an entry updated when a player mine a block of the given {@link Material}.
+	 * 
+	 * @param score      The line number of this entry.
+	 * @param entityType The type of entity used to update this entry.
+	 * @param before     The sequence of characters to be displayed before the player statistic.
+	 */
+	public AutoUpdateMinedBlockEntry(int score, Material material, String before) {
+		super(score, Statistic.MINE_BLOCK, material, before);
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onPlayerMineBlock(BlockBreakEvent event) {
+		if (getMaterial().equals(event.getBlock().getType()))
+			update();
+	}
+}
