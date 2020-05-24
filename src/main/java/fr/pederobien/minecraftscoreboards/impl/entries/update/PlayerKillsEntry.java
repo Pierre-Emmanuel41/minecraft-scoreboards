b9f1b45731cbb0a4b0f7c0e0925b772a8dc1dcf8
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-24 16:07:11 +0200

    Adding class PlayerKillsEntry

---
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/entries/update/PlayerKillsEntry.java
@@ -0,0 +1,27 @@
+package fr.pederobien.minecraftscoreboards.impl.entries.update;
+
+import org.bukkit.Statistic;
+
+public class PlayerKillsEntry extends StatisticEntry {
+
+	/**
+	 * Create an entry that is updated when a player kills another player.
+	 * 
+	 * @param score  The line number of this entry.
+	 * @param before The sequence of characters to be displayed before the player kill number.
+	 */
+	public PlayerKillsEntry(int score, String before) {
+		super(score, Statistic.PLAYER_KILLS, before);
+	}
+
+	/**
+	 * Create an entry that is updated when a player kills another player.
+	 * 
+	 * @param score  The line number of this entry.
+	 * @param before The sequence of characters to be displayed before the player kill number.
+	 * @param after  The sequence of characters to be displayed after the player kill number.
+	 */
+	public PlayerKillsEntry(int score, String before, String after) {
+		super(score, Statistic.PLAYER_KILLS, before, after);
+	}
+}
