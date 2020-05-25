199e42db2121ea38da584f431b583107d2ef45c6
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-25 19:29:58 +0200

    Upgrading interface IEntriesObjective

--- src/main/java/fr/pederobien/minecraftscoreboards/impl/EntriesObjective.java
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/EntriesObjective.java
@@ -5,16 +5,22 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
+import fr.pederobien.minecraftmanagers.ScoreboardManager;
 import fr.pederobien.minecraftscoreboards.interfaces.IEntriesObjective;
 import fr.pederobien.minecraftscoreboards.interfaces.IEntry;
 
 public class EntriesObjective<T extends IEntry> extends SimpleObjective implements IEntriesObjective<T> {
 	private Map<Integer, T> entries;
 	private List<T> entriesList;
+	private Scoreboard scoreboard;
+	private Objective objective;
 
 	/**
 	 * Create an empty objective based on the given parameters.
@@ -69,5 +75,47 @@ public class EntriesObjective<T extends IEntry> extends SimpleObjective implemen
 	@Override
 	public List<T> entries() {
 		return entriesList;
+	}
+
+	@Override
+	public void setScoreboard(Scoreboard scoreboard) {
+		this.scoreboard = scoreboard;
+		if (scoreboard == null)
+			return;
+
+		objective = ScoreboardManager.createObjective(scoreboard, getName(), getCriteria(), getDisplayName(), getDisplaySlot());
+	}
+
+	@Override
+	public Optional<Scoreboard> getScoreboard() {
+		return scoreboard == null ? Optional.empty() : Optional.of(scoreboard);
+	}
+
+	@Override
+	public Optional<Objective> getObjective() {
+		return objective == null ? Optional.empty() : Optional.of(objective);
+	}
+
+	@Override
+	public void update() {
+		Optional<Scoreboard> optScoreboard = getScoreboard();
+		if (!optScoreboard.isPresent())
+			return;
+
+		for (IEntry entry : entries.values())
+			updateEntry(entry, false);
+	}
+
+	@Override
+	public void update(IEntry entry) {
+		updateEntry(entry, true);
+	}
+
+	private void updateEntry(IEntry entry, boolean checkScoreboard) {
+		if (getPlayer() == null || checkScoreboard && !getScoreboard().isPresent())
+			return;
+		scoreboard.resetScores(entry.getOldValue());
+		entry.update(getPlayer());
+		objective.getScore(entry.getCurrentValue()).setScore(entry.getScore());
 	}
 }
--- src/main/java/fr/pederobien/minecraftscoreboards/interfaces/IEntriesObjective.java
+++ src/main/java/fr/pederobien/minecraftscoreboards/interfaces/IEntriesObjective.java
@@ -1,6 +1,10 @@
 package fr.pederobien.minecraftscoreboards.interfaces;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
 public interface IEntriesObjective<T extends IEntry> extends ISimpleObjective {
 
@@ -23,4 +27,39 @@ public interface IEntriesObjective<T extends IEntry> extends ISimpleObjective {
 	 * @return A list that contains all registered entries for this objective. This list is unmodifiable.
 	 */
 	List<T> entries();
+
+	/**
+	 * Set the scoreboard of this objective. This objective is also attached to this scoreboard.
+	 * 
+	 * @param scoreboard The scoreboard attached to this objective.
+	 */
+	void setScoreboard(Scoreboard scoreboard);
+
+	/**
+	 * Gets the scoreboard to which this objective is attached if it exist.
+	 * 
+	 * @return An optional that contains the scoreboard to which this objective is attached if it exist, an empty optional otherwise.
+	 */
+	Optional<Scoreboard> getScoreboard();
+
+	/**
+	 * Get the bukkit objective equivalent of this objective.
+	 * 
+	 * @return An optional that contains the bukkit objective equivalent if this objective is attached to a scoreboard, an empty
+	 *         optional otherwise.
+	 */
+	Optional<Objective> getObjective();
+
+	/**
+	 * Update this objective for the tracked player. This method reset the objective for each entry old value and then display each
+	 * entry current value.
+	 */
+	void update();
+
+	/**
+	 * Update this objective for the tracked player and the specified entry.
+	 * 
+	 * @param entry The entry to update in this objective.
+	 */
+	void update(T entry);
 }
