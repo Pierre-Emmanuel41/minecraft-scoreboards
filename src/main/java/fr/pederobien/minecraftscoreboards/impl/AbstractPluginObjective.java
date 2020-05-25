48b4fd5f26cef29743c8d4449693ee8b225f6054
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-25 20:56:35 +0200

    Adding class AbstractPluginObjective

---
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/AbstractPluginObjective.java
@@ -0,0 +1,56 @@
+package fr.pederobien.minecraftscoreboards.impl;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scoreboard.DisplaySlot;
+
+import fr.pederobien.minecraftscoreboards.interfaces.IAutoUpdateEntry;
+import fr.pederobien.minecraftscoreboards.interfaces.IPluginObjective;
+
+public abstract class AbstractPluginObjective extends AbstractObjective<IAutoUpdateEntry> implements IPluginObjective {
+	private Plugin plugin;
+
+	/**
+	 * Create an empty objective based on the given parameters.
+	 * 
+	 * @param player      The player associated to this objective. This player is used to display its informations.
+	 * @param name        The name of this objective.
+	 * @param displayName The name displayed on the given player score board.
+	 */
+	public AbstractPluginObjective(Player player, String name, String displayName, Plugin plugin) {
+		super(player, name, displayName);
+		this.plugin = plugin;
+	}
+
+	/**
+	 * Create an empty objective based on the given parameters.
+	 * 
+	 * @param player      The player associated to this objective. This player is used to display its informations.
+	 * @param name        The name of this objective.
+	 * @param displayName The name displayed on the given player score board.
+	 * @param displaySlot The slot where this objective is displayed on player screen.
+	 */
+	public AbstractPluginObjective(Player player, String name, String displayName, DisplaySlot displaySlot, Plugin plugin) {
+		super(player, name, displayName, displaySlot);
+		this.plugin = plugin;
+	}
+
+	/**
+	 * Create an empty objective based on the given parameters.
+	 * 
+	 * @param player      The player associated to this objective. This player is used to display its informations.
+	 * @param name        The name of this objective.
+	 * @param displayName The name displayed on the given player score board.
+	 * @param criteria    The criteria tracked by this objective.
+	 * @param displaySlot The slot where this objective is displayed on player screen.
+	 */
+	public AbstractPluginObjective(Player player, String name, String displayName, String criteria, DisplaySlot displaySlot, Plugin plugin) {
+		super(player, name, displayName, criteria, displaySlot);
+		this.plugin = plugin;
+	}
+
+	@Override
+	public Plugin getPlugin() {
+		return plugin;
+	}
+}
