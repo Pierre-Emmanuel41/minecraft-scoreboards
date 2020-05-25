31be5988a053b23e73e6de03dd8139d4f2757df0
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-25 23:37:29 +0200

    Renaming MinedBlockEntry in package entries.update as AutoUpdateMinedBlockEntry

--- src/main/java/fr/pederobien/minecraftscoreboards/impl/entries/update/AutoUpdateMinedBlockEntry.java
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/entries/update/AutoUpdateMinedBlockEntry.java
@@ -1,14 +1,13 @@
 package fr.pederobien.minecraftscoreboards.impl.entries.update;
 
 import org.bukkit.Material;
-import org.bukkit.Statistic;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import fr.pederobien.minecraftscoreboards.impl.entries.common.StatisticEntry;
+import fr.pederobien.minecraftscoreboards.impl.entries.common.MinedBlockEntry;
 
-public class MinedBlockEntry extends StatisticEntry {
+public class AutoUpdateMinedBlockEntry extends MinedBlockEntry {
 
 	/**
 	 * Create an entry updated when a player mine a block of the given {@link Material}.
@@ -18,8 +17,8 @@ public class MinedBlockEntry extends StatisticEntry {
 	 * @param before     The sequence of characters to be displayed before the player statistic.
 	 * @param after      The sequence of characters to be displayed after the player statistic.
 	 */
-	public MinedBlockEntry(int score, Material material, String before, String after) {
-		super(score, Statistic.MINE_BLOCK, material, before, after);
+	public AutoUpdateMinedBlockEntry(int score, Material material, String before, String after) {
+		super(score, material, before, after);
 	}
 
 	/**
@@ -29,8 +28,8 @@ public class MinedBlockEntry extends StatisticEntry {
 	 * @param entityType The type of entity used to update this entry.
 	 * @param before     The sequence of characters to be displayed before the player statistic.
 	 */
-	public MinedBlockEntry(int score, Material material, String before) {
-		super(score, Statistic.MINE_BLOCK, material, before);
+	public AutoUpdateMinedBlockEntry(int score, Material material, String before) {
+		super(score, material, before);
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
