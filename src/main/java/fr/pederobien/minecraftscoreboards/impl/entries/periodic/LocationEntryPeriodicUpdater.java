329e7553cb7bf10d36e67d9e57801797c81f49cd
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-27 19:15:03 +0200

    Adding class LocationEntryPeriodicUpdater

---
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/entries/periodic/LocationEntryPeriodicUpdater.java
@@ -0,0 +1,144 @@
+package fr.pederobien.minecraftscoreboards.impl.entries.periodic;
+
+import org.bukkit.plugin.Plugin;
+
+import fr.pederobien.minecraftscoreboards.impl.PeriodicUpdater;
+import fr.pederobien.minecraftscoreboards.impl.entries.common.LocationEntry;
+import fr.pederobien.minecraftscoreboards.interfaces.ISimpleObjective;
+
+public class LocationEntryPeriodicUpdater extends PeriodicUpdater<LocationEntry> {
+
+	/**
+	 * Create an entry updater. This entry is responsible to update the source entry.
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param delay     Represents the number of server ticks to wait before updating the objective.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param source    The source tracked by this updater.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long delay, long period, LocationEntry source) {
+		super(plugin, objective, delay, period, source);
+	}
+
+	/**
+	 * Create an entry updater. This entry is responsible to update the source entry.
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param source    The source tracked by this updater.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long period, LocationEntry source) {
+		this(plugin, objective, 0, period, source);
+	}
+
+	/**
+	 * Create an entry that displays the player location. The message associated to this entry is :</br>
+	 * Location loc = player.getLocation();</br>
+	 * return before + loc.getBlockX() + delimiter + loc.getBlockY() + delimiter + loc.getBlockZ()</br>
+	 * This constructor use " " as default delimiter.
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param delay     Represents the number of server ticks to wait before updating the objective.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param score     The line number of this entry.
+	 * @param before    The sequence of characters to be displayed before the player location. See above.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long delay, long period, int score, String before) {
+		this(plugin, objective, delay, period, new LocationEntry(score, before));
+	}
+
+	/**
+	 * Create an entry that displays the player location. The message associated to this entry is :</br>
+	 * Location loc = player.getLocation();</br>
+	 * return before + loc.getBlockX() + delimiter + loc.getBlockY() + delimiter + loc.getBlockZ()</br>
+	 * This constructor use " " as default delimiter.
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param score     The line number of this entry.
+	 * @param before    The sequence of characters to be displayed before the player location. See above.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long period, int score, String before) {
+		this(plugin, objective, 0, period, new LocationEntry(score, before));
+	}
+
+	/**
+	 * Create an entry that displays the player location. The message associated to this entry is :</br>
+	 * Location loc = player.getLocation();</br>
+	 * return before + loc.getBlockX() + delimiter + loc.getBlockY() + delimiter + loc.getBlockZ()
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param delay     Represents the number of server ticks to wait before updating the objective.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param score     The line number of this entry.
+	 * @param before    The sequence of characters to be displayed before the player location. See above.
+	 * @param delimiter The sequence of characters to be displayed between each element added. See above.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long delay, long period, int score, String before, String delimiter) {
+		this(plugin, objective, delay, period, new LocationEntry(score, before, delimiter));
+	}
+
+	/**
+	 * Create an entry that displays the player location. The message associated to this entry is :</br>
+	 * Location loc = player.getLocation();</br>
+	 * return before + loc.getBlockX() + delimiter + loc.getBlockY() + delimiter + loc.getBlockZ()
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param score     The line number of this entry.
+	 * @param before    The sequence of characters to be displayed before the player location. See above.
+	 * @param delimiter The sequence of characters to be displayed between each element added. See above.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long period, int score, String before, String delimiter) {
+		this(plugin, objective, 0, period, new LocationEntry(score, before, delimiter));
+	}
+
+	/**
+	 * Create an entry that displays the player location. The message associated to this entry is :</br>
+	 * Location loc = player.getLocation();</br>
+	 * return before + loc.getBlockX() + delimiter + loc.getBlockY() + delimiter + loc.getBlockZ() + after;</br>
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param delay     Represents the number of server ticks to wait before updating the objective.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param score     The line number of this entry.
+	 * @param before    The sequence of characters to be displayed before the player location. See above.
+	 * @param delimiter The sequence of characters to be displayed between each element added. See above.
+	 * @param after     The sequence of characters to be displayed after the player location. See above.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long delay, long period, int score, String before, String delimiter, String after) {
+		this(plugin, objective, delay, period, new LocationEntry(score, before, delimiter, after));
+	}
+
+	/**
+	 * Create an entry that displays the player location. The message associated to this entry is :</br>
+	 * Location loc = player.getLocation();</br>
+	 * return before + loc.getBlockX() + delimiter + loc.getBlockY() + delimiter + loc.getBlockZ() + after;</br>
+	 * 
+	 * @param plugin    The plugin of this objective. The plugin could be useful to register its entries as event listener of to
+	 *                  create periodic entry update.
+	 * @param objective The objective associated to the source entry.
+	 * @param period    Represents the number of server ticks between two objective updates.
+	 * @param score     The line number of this entry.
+	 * @param before    The sequence of characters to be displayed before the player location. See above.
+	 * @param delimiter The sequence of characters to be displayed between each element added. See above.
+	 * @param after     The sequence of characters to be displayed after the player location. See above.
+	 */
+	public LocationEntryPeriodicUpdater(Plugin plugin, ISimpleObjective objective, long period, int score, String before, String delimiter, String after) {
+		this(plugin, objective, 0, period, new LocationEntry(score, before, delimiter, after));
+	}
+}
