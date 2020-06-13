df6828aa3272e552c7a471ff330d1e354df209ac
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-13 11:39:55 +0200

    Deleting interface IEntriesObjective

--- src/main/java/fr/pederobien/minecraftscoreboards/impl/Objective.java
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/Objective.java
@@ -1,5 +1,12 @@
 package fr.pederobien.minecraftscoreboards.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Scoreboard;
 
 import fr.pederobien.minecraftmanagers.BukkitManager;
 import fr.pederobien.minecraftmanagers.ScoreboardManager;
@@ -15,11 +23,13 @@ import fr.pederobien.minecraftmanagers.TeamManager;
 import fr.pederobien.minecraftscoreboards.interfaces.IEntry;
 import fr.pederobien.minecraftscoreboards.interfaces.IObjective;
 
-public class Objective extends EntriesObjective implements IObjective, Listener {
+public class Objective extends AbstractSimpleObjective implements IObjective, Listener {
+	private Map<Integer, ExtendedEntry> entries;
+	private List<IEntry> entriesList;
 	private boolean isInitialized, isActivated;
 	private Runnable colorUpdater;
 	private ChatColor color;
-	private int taskId;
+	private int emptyEntryCount, taskId;
 
 	/**
 	 * Create an empty objective based on the given parameters.
@@ -58,13 +68,31 @@ public class Objective extends EntriesObjective implements IObjective, Listener 
 	 */
 	public Objective(Plugin plugin, Player player, String name, String displayName, String criteria, DisplaySlot displaySlot) {
 		super(plugin, player, name, displayName, criteria, displaySlot);
+		entries = new HashMap<Integer, ExtendedEntry>();
+		entriesList = Collections.unmodifiableList(new ArrayList<IEntry>(entries.values()));
 		isInitialized = false;
 		isActivated = false;
 		colorUpdater = new ColorUpdater();
+		emptyEntryCount = 0;
 	}
 
 	@Override
-	public final void start() {
+	public void update() {
+		Optional<Scoreboard> optScoreboard = getScoreboard();
+		if (!optScoreboard.isPresent())
+			return;
+
+		for (IEntry entry : entries.values())
+			updateEntry(entry, false);
+	}
+
+	@Override
+	public void update(IEntry entry) {
+		updateEntry(entry, true);
+	}
+
+	@Override
+	public void start() {
 		ScoreboardManager.setPlayerScoreboard(getPlayer(), getScoreboard().get());
 		setActivated(true);
 		update();
@@ -78,11 +106,43 @@ public class Objective extends EntriesObjective implements IObjective, Listener 
 	}
 
 	@Override
-	public final void initialize() {
+	public void addEntry(IEntry entry) {
+		entries.put(entry.getScore(), new ExtendedEntry(entry, false));
+		entriesList = Collections.unmodifiableList(new ArrayList<IEntry>(entries.values()));
+	}
+
+	@Override
+	public void removeEntry(int score) {
+		ExtendedEntry entry = entries.remove(score);
+		if (entry == null)
+			return;
+
+		if (entry.isEmpty())
+			emptyEntryCount--;
+
+		entriesList = Collections.unmodifiableList(new ArrayList<IEntry>(entries.values()));
+	}
+
+	@Override
+	public void emptyEntry(int score) {
+		String spaces = " ";
+		for (int i = 0; i < emptyEntryCount; i++)
+			spaces = spaces.concat(" ");
+		emptyEntryCount++;
+		addEntry(new ExtendedEntry(new MessageEntry(score, spaces), true));
+	}
+
+	@Override
+	public List<IEntry> entries() {
+		return entriesList;
+	}
+
+	@Override
+	public void initialize() {
 		if (isInitialized)
 			return;
 
-		action(entry -> entry.initialize());
+		entries().forEach(entry -> entry.initialize());
 		isInitialized = true;
 	}
 
@@ -94,13 +154,34 @@ public class Objective extends EntriesObjective implements IObjective, Listener 
 	@Override
 	public void setActivated(boolean isActivated) {
 		this.isActivated = isActivated;
-		action(entry -> entry.setActivated(isActivated));
+		entries().forEach(entry -> entry.setActivated(isActivated));
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
 		if (isActivated && event.getPlayer().getName().equals(event.getPlayer().getName()))
 			setPlayer(event.getPlayer());
+	}
+
+	private void updateEntry(IEntry entry, boolean checkScoreboard) {
+		if (getPlayer() == null || checkScoreboard && !getScoreboard().isPresent())
+			return;
+		getScoreboard().get().resetScores(entry.getOldValue());
+		entry.update(getPlayer());
+		getObjective().get().getScore(entry.getCurrentValue()).setScore(entry.getScore());
+	}
+
+	private class ExtendedEntry extends EntryWrapper<IEntry> {
+		private boolean isEmpty;
+
+		public ExtendedEntry(IEntry source, boolean isEmpty) {
+			super(source);
+			this.isEmpty = isEmpty;
+		}
+
+		public boolean isEmpty() {
+			return isEmpty;
+		}
 	}
 
 	private class ColorUpdater implements Runnable {
--- src/main/java/fr/pederobien/minecraftscoreboards/interfaces/IObjective.java
+++ src/main/java/fr/pederobien/minecraftscoreboards/interfaces/IObjective.java
@@ -1,6 +1,35 @@
 package fr.pederobien.minecraftscoreboards.interfaces;
 
-public interface IObjective extends ISimpleObjective, IEntriesObjective, IUpdateObjective {
+import java.util.List;
+
+public interface IObjective extends ISimpleObjective, IUpdateObjective {
+
+	/**
+	 * Append an entry to this objective. If an entry is already registered for the given {@link IEntry#getScore()} then the former
+	 * entry is replaced.
+	 * 
+	 * @param entry The entry to add to the objective.
+	 */
+	void addEntry(IEntry entry);
+
+	/**
+	 * Remove the entry associated to the given score from the list of entry for this objective.
+	 * 
+	 * @param score the score, ie the line number, of the entry to remove.
+	 */
+	void removeEntry(int score);
+
+	/**
+	 * Create an empty entry and add it to the list of entry for this objective.
+	 * 
+	 * @param score The line number of the empty entry.
+	 */
+	void emptyEntry(int score);
+
+	/**
+	 * @return A list that contains all registered entries for this objective. This list is unmodifiable.
+	 */
+	List<IEntry> entries();
 
 	/**
 	 * Initialize each registered entry if not already initialized.
