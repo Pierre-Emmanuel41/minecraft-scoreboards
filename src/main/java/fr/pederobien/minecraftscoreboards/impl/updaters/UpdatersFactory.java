8c7cbcf98a137b92253aecd73e6455563e260449
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-02-04 17:19:30 +0100

    Adding documentation for UpdatersFactory

--- src/main/java/fr/pederobien/minecraftscoreboards/impl/updaters/UpdatersFactory.java
+++ src/main/java/fr/pederobien/minecraftscoreboards/impl/updaters/UpdatersFactory.java
@@ -14,26 +14,53 @@ import fr.pederobien.minecraftscoreboards.interfaces.IEventEntryUpdater;
 
 public class UpdatersFactory {
 
+	/**
+	 * Creates a periodic updater based on the given parameters.
+	 * 
+	 * @param delay  The number of server tick to wait before the first update.
+	 * @param period The number of server tick between two updates.
+	 * 
+	 * @return A periodic entry updater.
+	 */
 	public static IEntryUpdater periodic(long delay, long period) {
 		return new PeriodicEntryUpdater(delay, period);
 	}
 
+	/**
+	 * Creates a periodic updater based on the given parameters.
+	 * 
+	 * @param period The number of server tick between two updates.
+	 * 
+	 * @return A periodic entry updater.
+	 */
 	public static IEntryUpdater periodic(long period) {
 		return new PeriodicEntryUpdater(period);
 	}
 
+	/**
+	 * @return An updater that update the entry when a block is broken by the player associated to the entry.
+	 */
 	public static IEventEntryUpdater<BlockBreakEvent> blockBreak() {
 		return new BlockBreakUpdater();
 	}
 
+	/**
+	 * @return An updater that update the entry when the player associated to the entry takes damages.
+	 */
 	public static IEventEntryUpdater<EntityDamageEvent> entityDamage() {
 		return new EntityDamageUpdater();
 	}
 
+	/**
+	 * @return An updater that update the entry when the player associated to the entry dies.
+	 */
 	public static IEventEntryUpdater<EntityDeathEvent> entityDeath() {
 		return new EntityDeathUpdater();
 	}
 
+	/**
+	 * @return An updater that update the entry when the player associated to the entry regains health.
+	 */
 	public static IEventEntryUpdater<EntityRegainHealthEvent> entityRegainHealth() {
 		return new EntityRegainHealthUpdater();
 	}
@@ -42,10 +69,16 @@ public class UpdatersFactory {
 		return new PlayerGameModeChangeUpdater();
 	}
 
+	/**
+	 * @return An updater that update the entry when the player associated to the the entry moves.
+	 */
 	public static IEventEntryUpdater<PlayerMoveEvent> playerMove() {
 		return new PlayerMoveUpdater();
 	}
 
+	/**
+	 * @return An updater that update the entry when the player associated to the entry respawns.
+	 */
 	public static IEventEntryUpdater<PlayerRespawnEvent> playerRespawn() {
 		return new PlayerRespawnUpdater();
 	}
